3f274254f7cce4fbe7ee78a1a66a4cad284fd77c
khaledui <khaledui.72@gmail.com>
2022-04-11 15:00:11 +0530

    suggested video remove youtube

--- src/components/Video.js
+++ src/components/Video.js
@@ -26,7 +26,7 @@ const video = () => (
   // </ModalRoutingContext.Consumer>
 
   <div className='crest-video'>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/-lwCneFPQwo?controls=0&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/-lwCneFPQwo?rel=0&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay;  encrypted-media;" allowfullscreen></iframe>
   </div>
 )
 
